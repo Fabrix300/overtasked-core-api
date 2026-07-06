b74d7d44fd208c24d7bce06cb5bdfe7727e51158
Fabricio Sotelo <fsotelo@aynitech.com>
2026-07-05 22:57:52 -0500

    [feat] first commit

---
+++ src/main/java/com/overtasked/overtaskedcoreapi/OvertaskedCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.overtasked.overtaskedcoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertaskedCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvertaskedCoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/overtasked/overtaskedcoreapi/OvertaskedCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overtasked.overtaskedcoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertaskedCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
