8ccaad239c08e3f9c541c5156a399f7fa9075d94
Fabricio Sotelo <fsotelo@aynitech.com>
2026-09-07 23:38:35 -0500

    test: create user service

--- src/main/java/com/overtasked/overtaskedcoreapi/domain/model/user/Email.java
+++ src/main/java/com/overtasked/overtaskedcoreapi/domain/model/user/Email.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public record Email(String value) {
 
     private static final Pattern EMAIL_PATTERN =
-            Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,6}$");
+            Pattern.compile("^[A-Za-z0-9](?:[A-Za-z0-9._%+-]*[A-Za-z0-9])?@[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?(?:\\.[A-Za-z]{2,})+$");
 
     public Email {
         if (value == null || value.isBlank()) {
